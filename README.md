23853c4c715b5629f52ecc452703120ef36f79f3
Ramon Lisboa <julio.lisboa@oncologiador.corp>
2022-09-15 21:11:13 -0300

    Enviando Arquivos

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# dev2bluprojcsharpdoop
+# Projeto C# Object Oriented Programming
 Projeto OOP C# Programa +Devs2Blu 2022
+
+** Descrição do Projeto **
---
+++ SlnOOPProject/Dev2Blu.ProjetosAula.AulaGIT.OOPProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev2Blu.ProjetosAula.AulaGIT.OOPProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
